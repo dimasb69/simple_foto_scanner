062c068a364f83143a27a4ee3f1de2123a3c1b86
MomDontGoDev <dimas.brizuela@gmail.com>
2024-02-07 22:11:54 -0500

    App v1.0 terminada Simple scanner de imagenes y Fotos

---
+++ lib/functions_const.dart
@@ -0,0 +1,145 @@
+import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+
+
+import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+var stat = false;
+
+Future<bool> checkPermission(BuildContext context) async {
+
+  final androidInfo = await DeviceInfoPlugin().androidInfo;
+  var sdk = androidInfo.version.sdkInt.toString();
+  late final Map<Permission, PermissionStatus> statuses;
+  if (sdk == "33" || sdk == "34") {
+    statuses = await [Permission.manageExternalStorage, Permission.photos].request();
+  } else {
+    statuses = await [
+      Permission.manageExternalStorage,
+      Permission.storage
+    ].request();
+  }
+
+  var stat = false;
+  statuses.forEach((permission, status) {
+    //print(status);
+    if (status != PermissionStatus.granted) {
+      stat = false;
+    } else {
+      stat = true;
+    }
+  });
+  return stat;
+}
+
+var perm10 = 0;
+
+Future<void> checkWritePermission() async {
+  if (!kIsWeb) {
+    if (Platform.isAndroid || Platform.isIOS) {
+      var permissionStatus = await Permission.storage.status;
+
+      print('permission status: $permissionStatus');
+
+      switch (permissionStatus) {
+        case PermissionStatus.denied:
+          await Permission.storage.request();
+          if (await Permission.storage.status == PermissionStatus.granted){
+            perm10 = 1;
+          }
+          break;
+        case PermissionStatus.permanentlyDenied:
+          await Permission.storage.request();
+          if (await Permission.storage.status == PermissionStatus.granted){
+            perm10 = 1;
+          }
+          break;
+        default:
+      }
+    }
+  }
+}
+
+Future<bool> _checkPermission(BuildContext context) async {
+  final androidInfo = await DeviceInfoPlugin().androidInfo;
+  var sdk = androidInfo.version.sdkInt.toString();
+
+  late final Map<Permission, PermissionStatus> statusess;
+
+  if (sdk == "33") {
+    statusess =
+    await [Permission.manageExternalStorage, Permission.photos, Permission.storage].request();
+  } else {
+    statusess = await [
+      Permission.manageExternalStorage,
+      Permission.storage
+    ].request();
+  }
+
+  var stat = false;
+  statusess.forEach((permission, status) {
+    if (status != PermissionStatus.granted) {
+      stat = false;
+    } else {
+      stat = true;
+    }
+  });
+  return stat;
+}
+
+Widget bottomDevName() {
+  return SizedBox(
+    height: 20,
+    child: Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 0,
+            child: GestureDetector(
+              onTap: () async{
+                final Uri url = Uri.parse('https://momdontgo.dev');
+                if (!await launchUrl(url)) {
+                  throw Exception('Could not launch $url');
+                }
+              },
+              child: const Text('Developed',
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.blueGrey,
+                      fontWeight: FontWeight.bold)),
+            ),
+          ),
+          Expanded(
+            flex: 0,
+            child: GestureDetector(
+              onTap: ()async{
+                final Uri url = Uri.parse('https://momdontgo.dev');
+                if (!await launchUrl(url)) {
+                  throw Exception('Could not launch $url');
+                }
+              },
+              child: const Text('By {MomDontGo.Dev}',
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.blueGrey,
+                      fontWeight: FontWeight.bold)),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,249 @@
+import 'dart:io';
+
+
+import 'package:document_scanner_flutter/document_scanner_flutter.dart';
+import 'package:document_scanner_flutter/configs/configs.dart';
+import 'package:flutter/material.dart';
+import 'package:easy_pdf_viewer/easy_pdf_viewer.dart';
+import 'package:open_file_plus/open_file_plus.dart';
+import 'package:pdf_ocr/functions_const.dart';
+
+late bool docChk;
+late bool imgChk;
+late var fPath = "";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+
+}
+
+
+class _MyAppState extends State<MyApp> {
+  PDFDocument? _scannedDocument;
+  File? _scannedDocumentFile;
+  File? _scannedImage;
+
+  openPdfScanner(BuildContext context) async {
+    var doc = await DocumentScannerFlutter.launchForPdf(
+      context,
+      labelsConfig: {
+        ScannerLabelsConfig.ANDROID_NEXT_BUTTON_LABEL: "Next Steps",
+        ScannerLabelsConfig.PDF_GALLERY_FILLED_TITLE_SINGLE: "Only 1 Page",
+        ScannerLabelsConfig.PDF_GALLERY_FILLED_TITLE_MULTIPLE:
+        "Only {PAGES_COUNT} Page"
+      },
+      //source: ScannerFileSource.CAMERA
+    );
+    if (doc != null) {
+      var dateFormat = getDateNow();
+      _scannedDocument = null;
+      setState(() {});
+      await Future.delayed(const Duration(milliseconds: 100));
+      _scannedDocumentFile = doc;
+      var doc2 = await doc.copy('/storage/emulated/0/Documents/ScanPdf_$dateFormat.pdf');
+      _scannedDocumentFile = doc2;
+      _scannedDocument = await PDFDocument.fromFile(doc2);
+
+      setState(() {
+        docChk=true;
+        imgChk=false;
+        fPath = _scannedDocumentFile!.path;
+      });
+    }
+  }
+
+  openImageScanner(BuildContext context) async {
+    var image = await DocumentScannerFlutter.launch(context,
+        //source: ScannerFileSource.CAMERA,
+        labelsConfig: {
+          ScannerLabelsConfig.ANDROID_NEXT_BUTTON_LABEL: "Next Step",
+          ScannerLabelsConfig.ANDROID_OK_LABEL: "OK"
+        });
+    if (image != null) {
+      _scannedImage = image;
+      setState(() {
+        imgChk=true;
+        docChk=false;
+        fPath = _scannedImage!.path;
+      });
+    }
+
+  }
+@override
+  void initState() {
+    checkPermission(context);
+    checkWritePermission();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        appBarTheme:const AppBarTheme(
+          backgroundColor: Color(0xff95A2BD),
+        ),
+        scaffoldBackgroundColor: const Color(0xff4a5875),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Color(0xff95A2BD),
+                child: Image(image: AssetImage('assets/logo.png')),
+              ),
+              SizedBox(width: 12),
+              Text('Simple Foto Scanner', style: TextStyle(fontSize: 25, color: Colors.black45, fontWeight: FontWeight.bold))
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            if (_scannedDocument != null || _scannedImage != null) ...[
+              if (imgChk)
+                Expanded(
+                  child: Image.file(_scannedImage!,
+                      height: double.maxFinite,
+                      width: double.maxFinite,
+                      fit: BoxFit.fill),
+                ),
+              if (docChk)
+                Expanded(
+                    child: PDFViewer(
+                      document: _scannedDocument!,
+                    )),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Text(fPath, style: const TextStyle(fontSize: 8, color: Colors.grey),),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                                (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.pressed)) {
+                                return Theme.of(context).colorScheme.primary.withOpacity(0.5);
+                              }
+                            },
+                          ),
+                        ),
+                        child: const Text('Open File', style: TextStyle(fontSize: 16, color: Colors.blueGrey),),
+                        onPressed: () async{
+                          var openResult = 'Unknown';
+                          final pathFile = fPath;
+                          print('path: $pathFile');
+                          final path = pathFile;
+                          bool permissionReady = await checkPermission(context);
+                          if (permissionReady) {
+                          final result = await OpenFile.open(path);
+                          setState(() {
+                            result;
+                            //print('Tipo: ${result.type}');
+                            //print('Mensaje: ${result.message}');
+                            openResult = "type=${result.type}  message=${result.message}";
+                          });
+                          }
+                        },
+                          ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Builder(builder: (context) {
+                      return ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                                (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.pressed)) {
+                                return Theme.of(context).colorScheme.primary.withOpacity(0.5);
+                              }
+                            },
+                          ),
+                        ),
+                        onPressed: () => openPdfScanner(context),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: 25,
+                              height: 25,
+                              child: CircleAvatar(
+                                child: Image(image: AssetImage('assets/pdf.png')),
+                              ),
+                            ),
+                            SizedBox(width: 20),
+                            Text("Scan to PDF", style: TextStyle(fontSize: 16, color: Colors.black54),)
+                          ],
+                        )
+                      );
+                    }),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    flex: 1,
+                    child: Builder(builder: (context) {
+                      return ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                                  (Set<MaterialState> states) {
+                                if (states.contains(MaterialState.pressed)) {
+                                  return Theme.of(context).colorScheme.primary.withOpacity(0.5);
+                                }
+                              },
+                            ),
+                          ),
+                          onPressed: () => openImageScanner(context),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                width: 25,
+                                height: 25,
+                                child: CircleAvatar(
+                                  child: Image(image: AssetImage('assets/jpg.png')),
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              Text("Scan to Image", style: TextStyle(fontSize: 16, color: Colors.black54),)
+                            ],
+                          )
+                      );
+                    }),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: bottomDevName(),
+      ),
+    );
+  }
+
+
+  String getDateNow () {
+    var d = DateTime.now();
+    final hours = d.hour.toString().padLeft(2, '0');
+    final minutes = d.minute.toString().padLeft(2, '0');
+    final seconds = d.second.toString().padLeft(2, '0');
+    var date = '${d.day}${d.month}${d.year}$hours$minutes$seconds';
+    return date.toString();
+  }
+
+}
